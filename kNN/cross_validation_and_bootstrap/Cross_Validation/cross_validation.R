fdf02f81e92d03a618cecf4635a8076cec6c80bc
yudhaputra <elekronikayudha@gmail.com>
2023-08-03 18:19:15 +0700

    belajar cari nilai k dgn cross validation

---
+++ kNN/cross_validation_and_bootstrap/Cross_Validation/cross_validation.R
@@ -0,0 +1,48 @@
+library(ISLR)
+library(e1071)
+
+
+normalize <- function(x) {
+  norm <- (x - min(x)) / (max(x) - min(x))
+  return(norm)
+}
+
+marketSample <- function(smarkt, test_pct = 20) {
+  train_pct = 100 - test_pct
+  
+  set.seed(test_pct)
+  test_indx <- sample(nrow(smarkt), nrow(smarkt) * test_pct / 100, replace = FALSE)
+  test.all <- smarkt[test_indx, ]  # Corrected: use input.norml instead of smarkt
+  test.data <- test.all[, -9]
+  test.direction <- test.all$Direction
+  
+  train.all <- smarkt[-test_indx, ]  # Corrected: use input.norml instead of smarkt
+  train.data <- train.all[, -9]
+  train.direction <- train.all$Direction
+  
+  rtrn <- list(testdata = test.data, testdir = test.direction, traindata = train.data, traindir = train.direction)
+  
+  return(rtrn)  # Corrected: removed _list from rtrn_list
+}
+
+
+input.norml <- as.data.frame(lapply(Smarket[, -9], normalize))
+input.norml$Direction <- Smarket$Direction
+
+full.data <- input.norml[, -9]
+full.dir <- input.norml[, 9]
+
+temp_list <- marketSample(input.norml, 20)
+train.data <- temp_list$traindata
+train.dir <- temp_list$traindir
+
+test.data <- temp_list$testdata
+test.dir <- temp_list$testdir
+
+rm(temp_list)
+
+knn.cross <- tune.knn(x = full.data, y = full.dir, k = 1:20, tunecontrol = tune.control(sampling = "boot"))
+
+summary(knn.cross)
+
+plot(knn.cross)
